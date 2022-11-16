fee2428e9ec693abe0c2b1af0b41bdd5d89070ce
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-11-17 00:24:41 +0700

    main script where the gameloop will live for now

---
+++ main.py
@@ -0,0 +1,45 @@
+import pygame, sys
+import asset as ASSETS
+import chess as CHESS
+
+
+
+
+
+pygame.init()
+window_size = pygame.math.Vector2(1040, 650)
+pygame.display.set_mode(window_size)
+bg_color = 'green'
+done = False
+scale_factor = 3
+
+
+game = CHESS.GAME(
+		board_asset = ASSETS.BOARDS.BOARD_PLAIN2,
+		piece_set = ASSETS.PIECE_SET.NORMAL16x32,
+		scale = scale_factor 
+	)
+
+white_p, black_p = game.pieces
+
+
+while not done:
+	
+	pygame.display.get_surface().fill(bg_color)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: done = True
+
+	pygame.display.get_surface().blit(game.board.sprite.surface, game.board.pos_rect)
+
+	CHESS.test_grid(game.board.grid)
+
+	a = 7 * scale_factor
+	for sprite in black_p:
+		game.board.sprite.surface.blit(sprite.surface, (a, 7 * scale_factor))
+		a += 16 * scale_factor
+
+	pygame.display.flip()
+
+pygame.quit()
+sys.exit()
