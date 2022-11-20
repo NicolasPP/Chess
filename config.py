6ee800eabcffa8c8cdf5bd2152205e2b9dd81c0e
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-11-21 03:20:51 +0700

    contant variables for FEN opperations

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 GRID_OFFSET : int = 7
 BOARD_SIZE : int = 8
 GAME_START_FEN : str = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-HOVER_COLOR : tuple[int, int, int] = 0,0,0
+FEN_BLANK = '@'
+FEN_SPLIT = '/'
