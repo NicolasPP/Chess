0b610b56f4bbd95958e98ac68cefb76e5a319ee7
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-11-21 14:30:39 +0700

    added type hints to config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 GRID_OFFSET : int = 7
 BOARD_SIZE : int = 8
 GAME_START_FEN : str = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-FEN_BLANK = '@'
-FEN_SPLIT = '/'
-NO_SURFACE = None
-PIECE_BG = 0,0,0
+FEN_BLANK : str = '@'
+FEN_SPLIT : str = '/'
+NO_SURFACE : None = None
+PIECE_BG : tuple[int, int, int] = 0,0,0
