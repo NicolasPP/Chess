1abc31ed837d72e68b4d2741f7597545e4b11920
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-11-22 12:18:22 +0700

    moved function which mutates fen string to fen script

--- FEN_notation.py
+++ FEN_notation.py
@@ -1,8 +1,8 @@
+import string
 from dataclasses import dataclass
 from typing import Generator
 
 from config import *
-import chess as CHESS
 
 
 @dataclass
@@ -12,17 +12,6 @@ class Fen:
 def iterate_FEN( fen : Fen ) -> Generator[str, None, None]:
 	for fen_row in fen.notation.split(FEN.SPLIT):
 		for piece_fen in fen_row: yield piece_fen 
-
-def decode_game_FEN( 
-		fen : Fen, 
-		player_pieces : dict[str, CHESS.Piece],
-		player_grid   : list[CHESS.Board_Square]
-	)-> Generator[tuple[CHESS.Piece, CHESS.Board_Square], None, None]:
-	count = 0
-	for piece_fen in iterate_FEN( fen ):
-		if piece_fen.isnumeric(): count += int( piece_fen ) -1
-		else: yield player_pieces[piece_fen], player_grid[count]
-		count += 1
 
 def set_blank_fen( fen : Fen = Fen(FEN.BLANK), blank_count : int = 0) -> tuple[Fen, int]:
 	if blank_count > 0:
@@ -37,7 +26,7 @@ def expand_fen( fen : Fen, expanded_fen : str = FEN.BLANK ) -> list[str]:
 		else: expanded_fen += piece_fen
 	return list(expanded_fen)
 
-def format_expanded_fen( unpacked_fen : list[str] ) -> Fen:
+def pack_fen( unpacked_fen : list[str] ) -> Fen:
 	fen, blank_count = set_blank_fen(Fen(FEN.BLANK))
 	for index in range(len( unpacked_fen )):
 		if index % BOARD_SIZE == 0 and index > 0:
@@ -48,3 +37,13 @@ def format_expanded_fen( unpacked_fen : list[str] ) -> Fen:
 			fen, blank_count = set_blank_fen( fen, blank_count) 
 			fen.notation += unpacked_fen[index]
 	return fen
+
+def make_move( from_coords, dest_coords, fen : Fen ) -> None:
+	expanded_fen = expand_fen(fen)
+	expanded_fen[fen_index(dest_coords)] = expanded_fen[fen_index(from_coords)]
+	expanded_fen[fen_index(from_coords)] = FEN.BLANK_PIECE
+	return pack_fen( expanded_fen )
+
+def fen_index( AN_coords ) -> int:
+	return ((BOARD_SIZE - int(AN_coords[0])) * BOARD_SIZE) + string.ascii_lowercase.index(AN_coords[1])
+
