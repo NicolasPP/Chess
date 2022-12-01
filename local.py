7c5edc33e7c0a1dcb78c1212eb6cc1eda97ef5f3
Nicolas Parucker <nico@Nicolass-MacBook-Pro.local>
2022-12-01 23:56:18 +0700

    changing the title of the window based on whos turn is

--- local.py
+++ local.py
@@ -11,10 +11,9 @@ from chess import match as MATCH
 
 
 pygame.init()
-window_size = pygame.math.Vector2(1040, 650)
+window_size = pygame.math.Vector2(WINDOW_SIZE)
 pygame.display.set_mode(window_size)
 done = False
-scale_factor = 4
 clock = pygame.time.Clock()
 
 match = MATCH.MATCH()
@@ -22,16 +21,21 @@ white_player = PLAYER.PLAYER(
 		side =CHESS.SIDE.WHITE,
 		piece_set = random.choice(list(ASSETS.PIECE_SET)),
 		board_asset = random.choice(list(ASSETS.BOARDS)),
-		scale = scale_factor
+		scale = BOARD_SCALE
 	)
 
 black_player = PLAYER.PLAYER(
 		side = CHESS.SIDE.BLACK,
 		piece_set = random.choice(list(ASSETS.PIECE_SET)),
 		board_asset = random.choice(list(ASSETS.BOARDS)),
-		scale = scale_factor
+		scale = BOARD_SCALE
 	)
 
+def update_window_caption(*players : PLAYER.Player) -> None:
+	for player in players:
+		if player.turn: 
+			pygame.display.set_caption(f"{player.side.name}s TURN")
+			return
 
 
 # placing boards in the middle of the screen
@@ -45,8 +49,8 @@ is_white = True
 while not done:
 	
 	fps = round(clock.get_fps())
-	bg_color = white_player.side.name.lower() if white_player.turn else black_player.side.name.lower()
-	font_color = white_player.side.name.lower() if not white_player.turn else black_player.side.name.lower()
+	bg_color = 'white' if white_player.turn else 'black'
+	font_color = 'black' if white_player.turn else 'white'
 	pygame.display.get_surface().fill(bg_color)
 
 	player = white_player if is_white else black_player
@@ -56,7 +60,7 @@ while not done:
 		if event.type == pygame.KEYDOWN: is_white =  not is_white
 		PLAYER.parse_local_player_input( event, player, match.fen )
 
-
+	update_window_caption(white_player, black_player)
 	MATCH.exec_player_command( match ) 
 	PLAYER.exec_match_command( white_player, black_player, match.fen ) 
 	
