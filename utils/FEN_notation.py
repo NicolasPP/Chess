b4437e93ec3ba3d96cc908f2ca7f89d6ee0574a2
Nicolas <nicolaspiolaparucker@gmail.com>
2023-01-16 15:39:03 +0700

    Created enum FEN_CHARS(enum.Enum) to hold some constant values

--- utils/FEN_notation.py
+++ utils/FEN_notation.py
@@ -1,13 +1,18 @@
-import string, dataclasses, typing
+import string, dataclasses, typing, enum
 
 from config import *
 
+
+class FEN_CHARS(enum.Enum):
+	BLANK_PIECE : str = "@"
+	SPLIT 		: str = '/'
+	BLANK 		: str = ''
 
 
 
 @dataclasses.dataclass
 class Fen:
-	notation : str = FEN.GAME_START_FEN
+	notation : str = GAME_START_FEN
 
 	def __getitem__(self, AN_C : str) -> int:
 		'''
@@ -24,7 +29,7 @@ class Fen:
 
 # -- Fen Helpers -- 
 def iterate_FEN( fen : Fen ) -> typing.Generator[str, None, None]:
-	for fen_row in fen.notation.split(FEN.SPLIT):
+	for fen_row in fen.notation.split(FEN_CHARS.SPLIT.value):
 		for piece_fen in fen_row: yield piece_fen 
 
 def set_blank_fen( fen : Fen, blank_count : int = 0) -> tuple[Fen, int]:
@@ -33,23 +38,23 @@ def set_blank_fen( fen : Fen, blank_count : int = 0) -> tuple[Fen, int]:
 		blank_count = 0
 	return fen, blank_count
 
-def expand_fen( fen : Fen, expanded_fen : str = FEN.BLANK ) -> list[str]:
+def expand_fen( fen : Fen, expanded_fen : str = FEN_CHARS.BLANK.value ) -> list[str]:
 	for piece_fen in iterate_FEN( fen ):
-		if piece_fen.isnumeric(): expanded_fen += (int( piece_fen ) * FEN.BLANK_PIECE)
-		elif piece_fen == FEN.SPLIT: continue
+		if piece_fen.isnumeric(): expanded_fen += (int( piece_fen ) * FEN_CHARS.BLANK_PIECE.value)
+		elif piece_fen == FEN_CHARS.SPLIT.value: continue
 		else: expanded_fen += piece_fen
 	
 	return list(expanded_fen)
 
 def pack_fen( unpacked_fen : list[str] ) -> Fen:
-	fen, blank_count = set_blank_fen(Fen(FEN.BLANK))
+	fen, blank_count = set_blank_fen(Fen(FEN_CHARS.BLANK.value))
 	for index in range(len( unpacked_fen )):
 
 		if index % BOARD_SIZE == 0 and index > 0:
 			fen, blank_count = set_blank_fen( fen, blank_count) 
-			fen.notation += FEN.SPLIT
+			fen.notation += FEN_CHARS.SPLIT.value
 
-		if unpacked_fen[index] == FEN.BLANK_PIECE: blank_count += 1
+		if unpacked_fen[index] == FEN_CHARS.BLANK_PIECE.value: blank_count += 1
 		else:
 			fen, blank_count = set_blank_fen( fen, blank_count) 
 			fen.notation += unpacked_fen[index]
@@ -62,7 +67,7 @@ def make_move( cmd_info : str, fen : Fen ) -> Fen:
 	from_c, dest_c, player = cmd_info.split(I_SPLIT)
 	expanded_fen = expand_fen(fen)
 	expanded_fen[fen[dest_c]] = expanded_fen[fen[from_c]]
-	expanded_fen[fen[from_c]] = FEN.BLANK_PIECE
+	expanded_fen[fen[from_c]] = FEN_CHARS.BLANK_PIECE.value
 	assert len( expanded_fen ) == BOARD_SIZE * BOARD_SIZE
 	return pack_fen( expanded_fen )
 # -----------------
