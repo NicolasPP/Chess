5c46146946f26540e682986cbeff8648fafc4895
Nicolas <nicolaspiolaparucker@gmail.com>
2023-01-20 11:57:06 +0700

    removed fen string for used for testing

--- config.py
+++ config.py
@@ -13,7 +13,6 @@ PIECE_BG : tuple[int, int, int] = 0,0,0
 
 # -- Default vals --
 GAME_START_FEN : str = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-CHECK_MATE_CHECK : str = 'rnbqkbnr/pppp1ppp/8/4p3/8/5PP1/PPPPP11P/RNBQKBNR'
 GRID_OFFSET : int = 7
 NO_SURFACE : None = None
 EMPTY_Q : None  = None
