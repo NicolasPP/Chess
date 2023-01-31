86d31cd6f4f609faa8206452df2ecb362eeb30a8
Nicolas <nicolaspiolaparucker@gmail.com>
2023-01-31 21:00:26 +0700

    reformatting code

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 # -- Alpha --
-AVAILABLE_ALPHA : int = 70
+AVAILABLE_ALPHA: int = 70
 
 # -- Size --
-WINDOW_SIZE : tuple[int,int] = 570, 570
-BOARD_SCALE : float = 3.5
-BOARD_SIZE : int = 8
-DATA_SIZE : int = 4096
+WINDOW_SIZE: tuple[int, int] = 570, 570
+BOARD_SCALE: float = 3.5
+BOARD_SIZE: int = 8
+DATA_SIZE: int = 4096
 
 # -- Colors --
-AVAILABLE_MOVE_COLOR : str = 'green'
-PIECE_BG : tuple[int, int, int] = 0,0,0
+AVAILABLE_MOVE_COLOR: str = 'green'
+PIECE_BG: tuple[int, int, int] = 0, 0, 0
 
 # -- Default vals --
-GAME_START_FEN : str = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
-GRID_OFFSET : int = 7
-NO_SURFACE : None = None
-EMPTY_Q : None  = None
+GAME_START_FEN: str = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+GRID_OFFSET: int = 7
+NO_SURFACE: None = None
+EMPTY_Q: None = None
 
 # -- Data operations --
-END_MARKER : str = '#'
-NO_INFO : str = '<>'
-I_SPLIT : str = '-'
-C_SPLIT : str = '%'
+END_MARKER: str = '#'
+NO_INFO: str = '<>'
+I_SPLIT: str = '-'
+C_SPLIT: str = '%'
