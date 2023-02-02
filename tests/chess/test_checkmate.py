24b06df8e3a639addaaba2001adc3c33d6dcc9ed
Nicolas <nicolaspiolaparucker@gmail.com>
2023-02-02 22:36:52 +0700

    added is_opponent_in_checkmate to testing checkmate

--- tests/chess/test_checkmate.py
+++ tests/chess/test_checkmate.py
@@ -13,8 +13,4 @@ import utils.FEN_notation as FEN
     (FEN.Fen('6Rk/8/5N2/8/8/8/8/8'), False)
 ])
 def test_checkmate(fen: FEN.Fen, is_white_turn: bool):
-    for index, fen_val in enumerate(fen.expanded):
-        if fen_val is FEN.FenChars.BLANK_PIECE.value: continue
-        if fen_val.islower() if is_white_turn else fen_val.isupper(): continue
-        piece_name = CHESS.get_name_from_fen(fen_val)
-        assert len(GAME.get_available_moves(piece_name, index, fen, is_white_turn)) is 0
+    assert GAME.is_opponent_in_checkmate(fen, not is_white_turn)
