957e61364496d81acd1482b60e689c7ea14e4bac
Nicolas <nicolaspiolaparucker@gmail.com>
2023-02-03 23:35:14 +0700

    finished script for algebraic_notation and test_algebraic_notation

---
+++ tests/utils/test_algebraic_notation.py
@@ -0,0 +1,43 @@
+import pytest
+
+import utils.algebraic_notation as AN
+
+
+@pytest.mark.parametrize("index,expected", [
+    (56, 'a1'),
+    (57, 'b1'),
+    (58, 'c1'),
+    (59, 'd1'),
+    (60, 'e1'),
+    (61, 'f1'),
+    (62, 'g1'),
+    (63, 'h1')
+])
+def test_get_an_from_index(index: int, expected: str):
+    assert AN.get_an_from_index(index).data.coordinates == expected
+
+
+@pytest.mark.parametrize("index", [-1, 65])
+def test_invalid_get_an_from_index(index: int):
+    with pytest.raises(IndexError):
+        AN.get_an_from_index(index)
+
+
+@pytest.mark.parametrize("an,expected", [
+    ('a1', 56),
+    ('b1', 57),
+    ('c1', 58),
+    ('d1', 59),
+    ('e1', 60),
+    ('f1', 61),
+    ('g1', 62),
+    ('h1', 63)
+])
+def test_get_index_from_anc(an, expected):
+    assert AN.get_index_from_an(*an) is expected
+
+
+@pytest.mark.parametrize("file,rank", [('a', '-8'), ('8', 'b'), ('8', '8'), ('9', 'd'), ('8', 'x')])
+def test_validate_file_and_rank(file: str, rank: str):
+    with pytest.raises(Exception):
+        AN.get_index_from_an(file, rank)
---
+++ utils/algebraic_notation.py
@@ -0,0 +1,47 @@
+import dataclasses
+import string
+
+from config import *
+
+
+@dataclasses.dataclass
+class AlgebraicNotationData:
+    file: str
+    rank: str
+    coordinates: str
+    index: int
+
+
+class AlgebraicNotation:
+    def __init__(self, file: str, rank: str):
+        validate_file_and_rank(file, rank)
+        self.data = get_data(file, rank)
+
+
+def get_data(file: str, rank: str) -> AlgebraicNotationData:
+    coordinates = file + rank
+    index = get_index_from_an(file, rank)
+    return AlgebraicNotationData(file, rank, coordinates, index)
+
+
+def get_index_from_an(file: str, rank: str) -> int:
+    validate_file_and_rank(file, rank)
+    ascii_str = string.ascii_lowercase
+    return ((BOARD_SIZE - int(rank)) * BOARD_SIZE) + ascii_str.index(file)
+
+
+def validate_file_and_rank(file: str, rank: str) -> bool:
+    if len(file) != 1 or \
+            len(rank) != 1: raise Exception("cannot be empty or larger than 1 char")
+    if file.isnumeric(): raise Exception("col should be letter")
+    if not rank.isnumeric(): raise Exception("row should be number")
+    if int(rank) > BOARD_SIZE or file > 'h': raise Exception("invalid index")
+    return True
+
+
+def get_an_from_index(index: int) -> AlgebraicNotation:
+    if index < 0: raise IndexError("index cannot be negative")
+    if index > 63: raise IndexError("index cannot be above 63")
+    row_num, col_num = divmod(index, BOARD_SIZE)
+    ascii_str = string.ascii_lowercase
+    return AlgebraicNotation(ascii_str[col_num], str(BOARD_SIZE - row_num))
