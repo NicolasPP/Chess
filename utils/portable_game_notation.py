7e3b1e3575d725768113718f4be3299ba84da79e
Nicolas <nicolaspiolaparucker@gmail.com>
2023-02-08 13:17:46 +0700

    renamed some variables and fixed formatting

--- utils/portable_game_notation.py
+++ utils/portable_game_notation.py
@@ -6,7 +6,7 @@ TagPairs: typing.TypeAlias = dict[str, str]
 
 
 @dataclasses.dataclass
-class Move:
+class PGNMove:
     white_move: str
     black_move: str
     white_clock: str
@@ -16,7 +16,7 @@ class Move:
 @dataclasses.dataclass
 class Game:
     tag_pairs: TagPairs
-    moves: list[Move]
+    pgn_moves: list[PGNMove]
     result: str
 
 
@@ -28,17 +28,21 @@ class PortableGameNotation:
     def parse_pgn_file(self) -> list[Game]:
         games: list[Game] = []
         tag_pairs: TagPairs = {}
+
         with open(self.file_name, "r") as file:
+
             lines = file.read().split('\n')
+
             for line in lines:
                 if not line: continue
                 if is_tag_pair(line):
                     insert_tag_pair(tag_pairs, line)
                 else:
-                    games.append(Game(tag_pairs, *get_moves_and_result(line)))
+                    games.append(Game(tag_pairs, *get_pgn_moves_and_result(line)))
                     tag_pairs = {}
 
             file.close()
+
         return games
 
 
@@ -54,19 +58,20 @@ def insert_tag_pair(tag_pairs: TagPairs, line: str) -> None:
     tag_pairs[name] = val
 
 
-def get_moves_and_result(line: str) -> tuple[list[Move], str]:
+def get_pgn_moves_and_result(line: str) -> tuple[list[PGNMove], str]:
     split_line = '[0-9A-Za-z]+\.'
     move_list = re.split(split_line, line)
-    moves: list[Move] = []
+    pgn_moves: list[PGNMove] = []
     result: str = ''
     for move in move_list:
         if not move: continue
+
         curly_brackets_content_pattern = '\{[^}]*\}'
         move_set = re.sub(curly_brackets_content_pattern, '', move).split()
         time_set = re.findall(curly_brackets_content_pattern, move)
 
         if len(time_set) == 0: time_set = ['', '']
-        if len(time_set) == 1: time_set.append(' ')
+        if len(time_set) == 1: time_set.append('')
         if len(move_set) == 3:
             result = move_set.pop(-1)
         elif is_result(move_set[0]):
@@ -75,11 +80,13 @@ def get_moves_and_result(line: str) -> tuple[list[Move], str]:
         elif is_result(move_set[1]):
             result = move_set[1]
             move_set[1] = ''
-        moves.append(Move(*move_set, *time_set))
+
+        pgn_moves.append(PGNMove(*move_set, *time_set))
+
         assert len(move_set) == 2 or len(move_set) == 3, f'{move}  {line}'
         assert len(time_set) == 2, f'{move}  {time_set}'
     assert result != '', 'result not found'
-    return moves, result
+    return pgn_moves, result
 
 
 def is_result(move: str) -> bool:
