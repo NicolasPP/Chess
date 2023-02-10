a9642f86c030b8bb0659d80034a4e3849f2e8ee3
Nicolas <nicolaspiolaparucker@gmail.com>
2023-02-11 01:54:47 +0700

    script for testing and type checking

---
+++ test.sh
@@ -0,0 +1,51 @@
+MainTests()
+{
+  pytest tests/utils test/chess
+}
+
+OnlyIntegrated()
+{
+  pytest tests/test_with_played_games.py
+}
+
+TestAll()
+{
+  MainTests
+  OnlyIntegrated
+}
+
+InvalidOption()
+{
+	echo "------------ INVALID OPTION : [$1] ------------"
+	ValidOptions
+	exit 1
+}
+
+ValidOptions()
+{
+	echo "---------------- VALID OPTIONS ----------------"
+	  echo "	-a | --all        : run all tests"
+    echo "	-i | --integrated : run integrated tests only"
+    echo "	-t | --type-check : run mypy check"
+}
+
+TypeCheck()
+{
+  mypy . --check-untyped-defs --explicit-package-bases
+}
+
+
+no_args="true"
+while [ $# -gt 0 ] ; do
+  case $1 in
+    -a | --all) TestAll;;
+    -i | --integrated) OnlyIntegrated ;;
+    -t | --type-check) TypeCheck ;;
+	*) InvalidOption $1;;
+
+  esac
+  no_args="false"
+  shift
+done
+
+[[ "$no_args" == "true" ]] && { MainTests; exit 1; }
