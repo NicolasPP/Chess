6e6b6f15b4260962c21b7b69b351c09ccab77720
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-14 22:43:33 +0700

    fixed test rook to fit with the new refactor

--- tests/chess/test_rook_moves.py
+++ tests/chess/test_rook_moves.py
@@ -1,37 +1,37 @@
 import pytest
 
-import chess.game as GAME
-import utils.Forsyth_Edwards_notation as FEN
+from utils.Forsyth_Edwards_notation import Fen
+from chess.piece import get_available_moves
 
 
 def test_base_moves():
-    assert len(GAME.get_available_moves('ROOK', 36, FEN.Fen('8/8/8/8/8/8/8/8 b KQkq - 0 1'))) == 14
+    assert len(get_available_moves('R', 36, Fen('8/8/8/8/8/8/8/8 b KQkq - 0 1'))) == 14
 
 
 @pytest.mark.parametrize("from_index,is_white_turn", [(0, False), (7, False), (63, True), (56, True)])
 def test_default_fen(from_index: int, is_white_turn: bool):
-    assert len(GAME.get_available_moves('ROOK', from_index, FEN.Fen(), is_white_turn)) is 0
+    assert len(get_available_moves('R', from_index, Fen(), is_white_turn)) is 0
 
 
 def test_fully_blocked():
-    assert len(GAME.get_available_moves('ROOK', 36, FEN.Fen('8/8/8/4p3/3prp2/4p3/8/8 b KQkq - 0 1'))) is 0
+    assert len(get_available_moves('R', 36, Fen('8/8/8/4p3/3prp2/4p3/8/8 b KQkq - 0 1'))) is 0
 
 
 def test_possible_take():
-    fen = FEN.Fen('8/8/8/4P3/3PrP2/4P3/8/8 b KQkq - 0 1')
-    moves = GAME.get_available_moves('ROOK', 36, fen)
+    fen = Fen('8/8/8/4P3/3PrP2/4P3/8/8 b KQkq - 0 1')
+    moves = get_available_moves('R', 36, fen)
     assert len(moves) is 4
     for move in moves:
         assert fen[move].isupper()
 
 
 def test_blocked_with_possible_take():
-    fen = FEN.Fen('8/8/8/4P3/2PprpP1/4P3/8/8 b KQkq - 0 1')
-    moves = GAME.get_available_moves('ROOK', 36, fen)
+    fen = Fen('8/8/8/4P3/2PprpP1/4P3/8/8 b KQkq - 0 1')
+    moves = get_available_moves('R', 36, fen)
     assert len(moves) is 2
     for move in moves:
         assert fen[move].isupper()
 
 
 def test_blocked_take():
-    assert len(GAME.get_available_moves('ROOK', 36, FEN.Fen('8/8/4P3/4p3/2PprpP1/4p3/4P3/8 b KQkq - 0 1'))) is 0
+    assert len(get_available_moves('R', 36, Fen('8/8/4P3/4p3/2PprpP1/4p3/4P3/8 b KQkq - 0 1'))) is 0
