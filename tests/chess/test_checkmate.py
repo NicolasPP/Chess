c5a4b2f4c545e71712eb0fae6367a3a3b8656a82
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-14 21:09:13 +0700

    fixed test checkmate to fit with the new refactor

--- tests/chess/test_checkmate.py
+++ tests/chess/test_checkmate.py
@@ -1,15 +1,18 @@
 import pytest
 
-import chess.game as GAME
-import utils.Forsyth_Edwards_notation as FEN
+import chess.validate_move as validate_move
+from chess.piece import Pieces
+from utils.Forsyth_Edwards_notation import Fen
+
+Pieces.load_pieces_info()
 
 
 @pytest.mark.parametrize("fen", [
-    FEN.Fen('rnb1kbnr/pppp1ppp/4p3/8/6Pq/5P2/PPPPP2P/RNBQKBNR b KQkq - 0 1'),
-    FEN.Fen('3rkbnr/1p1bp3/1q1p3p/p5pQ/8/8/8/8 w KQkq - 0 1'),
-    FEN.Fen('R6k/R7/8/8/8/8/8/5K2 w KQkq - 0 1'),
-    FEN.Fen('r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/8/8 w KQkq - 0 1'),
-    FEN.Fen('6Rk/8/5N2/8/8/8/8/8 w KQkq - 0 1')
+    Fen('rnb1kbnr/pppp1ppp/4p3/8/6Pq/5P2/PPPPP2P/RNBQKBNR b KQkq - 0 1'),
+    Fen('3rkbnr/1p1bp3/1q1p3p/p5pQ/8/8/8/8 w KQkq - 0 1'),
+    Fen('R6k/R7/8/8/8/8/8/5K2 w KQkq - 0 1'),
+    Fen('r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/8/8 w KQkq - 0 1'),
+    Fen('6Rk/8/5N2/8/8/8/8/8 w KQkq - 0 1')
 ])
-def test_checkmate(fen: FEN.Fen):
-    assert GAME.is_opponent_in_checkmate(fen)
+def test_checkmate(fen: Fen):
+    assert validate_move.is_opponent_in_checkmate(fen)
