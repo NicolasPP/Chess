d49f3f6d8fc5d38f5453960d41b16a51c5866615
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-14 22:19:03 +0700

    added load_pieces_info fixture so tests can have access to the pieces information
    and removed pawn fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
-import utils.Forsyth_Edwards_notation as FEN
+from chess.piece import Pieces
 
 
 @pytest.fixture(scope="session")
-def pawn_test_fen() -> FEN.Fen: return FEN.Fen("8/5pp1/2p2P2/2P5/7p/1P2p3/2P1PP1P/8 w KQkq - 0 1")
+def load_pieces_info() -> None:
+    Pieces.load_pieces_info()
