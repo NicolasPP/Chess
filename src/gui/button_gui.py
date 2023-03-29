679b98b4e4bed619751023498e4958d5a19f6389
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-30 02:17:10 +0700

    added boolean hover to be able to turn hover on and off

--- src/gui/button_gui.py
+++ src/gui/button_gui.py
@@ -1,6 +1,8 @@
 import pygame
 import dataclasses
 
+from config import HOVER_ALPHA
+
 
 @dataclasses.dataclass
 class FontRenderInfo:
@@ -14,10 +16,11 @@ class ButtonGui:
         self.rect: pygame.rect.Rect = pygame.rect.Rect(0, 0, width, height)
         self.surface: pygame.surface.Surface = pygame.surface.Surface(self.rect.size)
         self.hover_surface: pygame.surface.Surface = pygame.surface.Surface(self.rect.size)
+        self.hover: bool = True
         self.font_info: FontRenderInfo | None = None
         self.surface.fill(bg_color)
         self.hover_surface.fill(bg_color)
-        self.hover_surface.set_alpha(70)
+        self.hover_surface.set_alpha(HOVER_ALPHA)
 
     def set_font(self, font_file: str, font_size: int, anti_alias: bool, font_color: tuple[int, int, int]) -> None:
         self.font_info = FontRenderInfo(pygame.font.Font(font_file, font_size), anti_alias, font_color)
@@ -28,7 +31,11 @@ class ButtonGui:
         label_pos: pygame.rect.Rect = label_surface.get_rect(center=self.rect.center)
         self.surface.blit(label_surface, label_pos)
 
+    def set_hover(self, hover: bool) -> None:
+        self.hover = hover
+
     def render(self) -> None:
         pygame.display.get_surface().blit(self.surface, self.rect)
+        if not self.hover: return
         if self.rect.collidepoint(pygame.mouse.get_pos()):
             pygame.display.get_surface().blit(self.hover_surface, self.rect)
