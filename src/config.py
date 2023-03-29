a6dc83761307c24fb8f1a0dcc14bd45dc7260275
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-29 19:49:04 +0700

    added constants for new end_game_gui.py, and for scaling available move surface

--- src/config.py
+++ src/config.py
@@ -1,22 +1,45 @@
 # -- Alpha --
-AVAILABLE_ALPHA: int = 70
+AVAILABLE_ALPHA: int = 100
 
 # -- Size --
 WINDOW_SIZE: tuple[int, int] = 570, 570
 BOARD_SCALE: float = 3.5
+AVAILABLE_MOVE_SCALE: float = 1/3
 BOARD_SIZE: int = 8
 DATA_SIZE: int = 4096
 TIMER_FONT_SIZE: int = 15
+DRAW_BUTTON_WIDTH: int = 60
+DRAW_BUTTON_HEIGHT: int = 30
+RESIGN_BUTTON_WIDTH: int = 60
+RESIGN_BUTTON_HEIGHT: int = 30
+OFFER_DRAW_FONT_SIZE: int = 17
+RESIGN_FONT_SIZE: int = 17
+END_GAME_GUI_SPACING: int = 5
+PIECE_ASSET_ROW: int = 1
+PIECE_ASSET_COL: int = 6
 
 # -- Colors --
-AVAILABLE_MOVE_COLOR: str = 'green'
+AVAILABLE_MOVE_COLOR: tuple[int, int, int] = 76, 155, 87
 PIECE_BG: tuple[int, int, int] = 0, 0, 0
+
+PLAIN1_BG: tuple[int, int, int] = (234, 240, 217)
+PLAIN1_FG: tuple[int, int, int] = (89, 96, 111)
+
+PLAIN2_BG: tuple[int, int, int] = (234, 240, 217)
+PLAIN2_FG: tuple[int, int, int] = (192, 196, 180)
+
+PLAIN3_BG: tuple[int, int, int] = (150, 162, 178)
+PLAIN3_FG: tuple[int, int, int] = (89, 96, 111)
+
+PLAIN4_BG: tuple[int, int, int] = (230, 234, 216)
+PLAIN4_FG: tuple[int, int, int] = (69, 77, 94)
+
 
 # -- Default vals --
 GAME_START_FEN: str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 GRID_OFFSET: int = 7
-NO_SURFACE: None = None
-EMPTY_Q: None = None
+OFFER_DRAW_LABEL: str = "draw"
+RESIGN_LABEL: str = "resign"
 
 # -- File Name --
 FONT_FILE: str = "assets/fonts/Oleaguid.ttf"
