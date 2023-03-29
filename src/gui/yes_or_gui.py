07cdc66da65cebd8f4cd9c6dd4954e7e4bcefab5
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-30 03:38:10 +0700

    simple yes or no gui to ask client if they are sure they want to resign, offer draw and reject and accept draw

---
+++ src/gui/yes_or_gui.py
@@ -0,0 +1,23 @@
+import pygame
+
+from gui.button_gui import ButtonGui
+
+from config import *
+
+
+class YesOrNoGui:
+    def __init__(self, board_rect: pygame.rect.Rect):
+        self.board_rect: pygame.rect.Rect = board_rect
+        self.yes: ButtonGui = ButtonGui(DRAW_BUTTON_WIDTH, DRAW_BUTTON_HEIGHT, AVAILABLE_MOVE_COLOR)
+        self.no: ButtonGui = ButtonGui(RESIGN_BUTTON_WIDTH, RESIGN_BUTTON_HEIGHT, (255, 255, 255))
+        self.result: bool | None = None
+
+    def render(self) -> None:
+        self.yes.render()
+        self.no.render()
+
+    def set_result(self, result: bool | None) -> None:
+        self.result = result
+
+    def recalculate_pos(self) -> None:
+        self.yes.rect.x += 100
