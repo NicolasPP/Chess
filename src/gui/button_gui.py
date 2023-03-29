324d028961d2b2f49c2ee4fe5f517bc23fca6633
Nicolas <nicolaspiolaparucker@gmail.com>
2023-03-29 20:17:07 +0700

    button reacts to mouse hover

--- src/gui/button_gui.py
+++ src/gui/button_gui.py
@@ -13,8 +13,11 @@ class ButtonGui:
     def __init__(self, width: int, height: int, bg_color: tuple[int, int, int]):
         self.rect: pygame.rect.Rect = pygame.rect.Rect(0, 0, width, height)
         self.surface: pygame.surface.Surface = pygame.surface.Surface(self.rect.size)
+        self.hover_surface: pygame.surface.Surface = pygame.surface.Surface(self.rect.size)
         self.font_info: FontRenderInfo | None = None
         self.surface.fill(bg_color)
+        self.hover_surface.fill(bg_color)
+        self.hover_surface.set_alpha(70)
 
     def set_font(self, font_file: str, font_size: int, anti_alias: bool, font_color: tuple[int, int, int]) -> None:
         self.font_info = FontRenderInfo(pygame.font.Font(font_file, font_size), anti_alias, font_color)
@@ -27,3 +30,5 @@ class ButtonGui:
 
     def render(self) -> None:
         pygame.display.get_surface().blit(self.surface, self.rect)
+        if self.rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.display.get_surface().blit(self.hover_surface, self.rect)
