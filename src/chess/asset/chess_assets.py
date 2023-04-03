3246774b3f081526741a96cea076cbe1eaabf94a
Nicolas <nicolas.parucker@hotmail.com>
2023-04-03 12:38:36 +0200

    simplified chess_assets.py script

--- src/chess/asset/chess_assets.py
+++ src/chess/asset/chess_assets.py
@@ -6,20 +6,9 @@ import pygame
 from config import *
 
 
-class TYPE(enum.Enum):
-    SPRITE: int = 0
-    SHEET: int = 1
-
-
-@dataclasses.dataclass
-class Asset:
-    file: str
-    a_type: TYPE
-
-
 @dataclasses.dataclass
 class BoardAsset:
-    asset: Asset
+    file: str
     background: tuple[int, int, int]
     foreground: tuple[int, int, int]
 
@@ -39,30 +28,21 @@ class BoardSprite:
 
 @dataclasses.dataclass
 class PieceSet:
-    white_asset: Asset
-    black_asset: Asset
+    white_assets_file: str
+    black_assets_file: str
 
 
 class BoardAssets(enum.Enum):
-    PLAIN1: BoardAsset = BoardAsset(Asset(PLAIN1_FILE, TYPE.SPRITE), PLAIN1_BG, PLAIN1_FG)
-    PLAIN2: BoardAsset = BoardAsset(Asset(PLAIN2_FILE, TYPE.SPRITE), PLAIN2_BG, PLAIN2_FG)
-    PLAIN3: BoardAsset = BoardAsset(Asset(PLAIN3_FILE, TYPE.SPRITE), PLAIN3_BG, PLAIN3_FG)
-    PLAIN4: BoardAsset = BoardAsset(Asset(PLAIN4_FILE, TYPE.SPRITE), PLAIN4_BG, PLAIN4_FG)
+    PLAIN1: BoardAsset = BoardAsset(PLAIN1_FILE, PLAIN1_BG, PLAIN1_FG)
+    PLAIN2: BoardAsset = BoardAsset(PLAIN2_FILE, PLAIN2_BG, PLAIN2_FG)
+    PLAIN3: BoardAsset = BoardAsset(PLAIN3_FILE, PLAIN3_BG, PLAIN3_FG)
+    PLAIN4: BoardAsset = BoardAsset(PLAIN4_FILE, PLAIN4_BG, PLAIN4_FG)
 
 
 class PieceSetAssets(enum.Enum):
-    SIMPLE16x16: PieceSet = PieceSet(
-        white_asset=Asset(SIMPLE16x16_PIECE_FILE_WHITE, TYPE.SHEET),
-        black_asset=Asset(SIMPLE16x16_PIECE_FILE_BLACK, TYPE.SHEET)
-    )
-    NORMAL16x16: PieceSet = PieceSet(
-        white_asset=Asset(NORMAL16x16_PIECE_FILE_WHITE, TYPE.SHEET),
-        black_asset=Asset(NORMAL16x16_PIECE_FILE_BLACK, TYPE.SHEET)
-    )
-    NORMAL16x32: PieceSet = PieceSet(
-        white_asset=Asset(NORMAL16x32_PIECE_FILE_WHITE, TYPE.SHEET),
-        black_asset=Asset(NORMAL16x32_PIECE_FILE_BLACK, TYPE.SHEET)
-    )
+    SIMPLE16x16: PieceSet = PieceSet(SIMPLE16x16_PIECE_FILE_WHITE, SIMPLE16x16_PIECE_FILE_BLACK)
+    NORMAL16x16: PieceSet = PieceSet(NORMAL16x16_PIECE_FILE_WHITE, NORMAL16x16_PIECE_FILE_BLACK)
+    NORMAL16x32: PieceSet = PieceSet(NORMAL16x32_PIECE_FILE_WHITE, NORMAL16x32_PIECE_FILE_BLACK)
 
 
 def scale(surface: pygame.surface.Surface, surface_scale: float) -> pygame.surface.Surface:
@@ -79,8 +59,8 @@ def sheet_surface_gen(surface_size: pygame.math.Vector2):
             yield surface, index
 
 
-def load_sprite_sheet(asset: Asset, asset_scale: float) -> list[Sprite]:
-    sheet_sprite = load_sprite(asset.file, sprite_scale=1)
+def load_sprite_sheet(asset_file: str, asset_scale: float) -> list[Sprite]:
+    sheet_sprite = load_sprite(asset_file, sprite_scale=1)
     sheet_dimensions = pygame.math.Vector2(PIECE_ASSET_COL, PIECE_ASSET_ROW)
     sheet_size = pygame.math.Vector2(sheet_sprite.surface.get_size()).elementwise()
     surface_size = sheet_size / sheet_dimensions
@@ -102,8 +82,9 @@ def load_sprite(file: str, sprite_scale: float) -> Sprite:
 
 
 def load_board(board_asset: BoardAsset, asset_scale: float) -> BoardSprite:
-    return BoardSprite(load_sprite(board_asset.asset.file, asset_scale), board_asset.background, board_asset.foreground)
+    return BoardSprite(load_sprite(board_asset.file, asset_scale), board_asset.background, board_asset.foreground)
 
 
 def load_piece_set(piece_set: PieceSet, piece_scale: float) -> tuple[list[Sprite], list[Sprite]]:
-    return load_sprite_sheet(piece_set.white_asset, piece_scale), load_sprite_sheet(piece_set.black_asset, piece_scale)
+    return load_sprite_sheet(piece_set.white_assets_file, piece_scale), \
+           load_sprite_sheet(piece_set.black_assets_file, piece_scale)
