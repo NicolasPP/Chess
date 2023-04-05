2d093de42d047945058ad2c4603ff9398bf977d7
Nicolas <nicolas.parucker@hotmail.com>
2023-04-05 12:19:38 +0200

    logging script to set up the logging

---
+++ src/chess_logging.py
@@ -0,0 +1,14 @@
+import logging
+from config import LOG_FORMAT, LOG_FILE_MODE
+formatter: logging.Formatter = logging.Formatter(LOG_FORMAT)
+
+
+def set_up_logging(name: str, log_file: str, log_level=logging.DEBUG) -> logging.Logger:
+    handler: logging.FileHandler = logging.FileHandler(log_file, mode=LOG_FILE_MODE)
+    handler.setFormatter(formatter)
+
+    logger: logging.Logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+
+    return logger
