0d4326c37436d837f5a64ed7a54de4ec9225cbc1
Nicolas <nicolas.parucker@hotmail.com>
2023-04-05 12:17:37 +0200

    added constants for logging

--- src/config.py
+++ src/config.py
@@ -94,3 +94,11 @@ MOUSECLICK_MIDDLE: int = 2
 MOUSECLICK_RIGHT: int = 3
 MOUSECLICK_SCROLL_UP: int = 4
 MOUSECLICK_SCROLL_DOWN: int = 5
+
+# -- Logging --
+SERVER_NAME: str = "server"
+SERVER_LOG_FILE: str = "../Chess/log/server.log"
+CLIENT_NAME: str = "client"
+CLIENT_LOG_FILE: str = "../Chess/log/client.log"
+LOG_FILE_MODE: str = "w"
+LOG_FORMAT: str = "%(asctime)s\t%(levelname)s\t%(message)s"
