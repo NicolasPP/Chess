48e74e15602f32f866801e34bc29f8b901cf6a6e
Nicolas <nicolas.parucker@hotmail.com>
2023-04-18 17:30:03 +0200

    added highlighting for the board axis values for whatever tile the player is hovering

--- src/gui/board_axis_gui.py
+++ src/gui/board_axis_gui.py
@@ -5,7 +5,13 @@ from chess.board.side import Side
 from chess.asset.asset_manager import AssetManager
 from chess.game.game_surface import GameSurface
 from chess.game.game_surface import GameSize
+from chess.board.board_tile import BoardTile
 from config import *
+
+
+class TileHover(typing.NamedTuple):
+    file: str
+    rank: str
 
 
 class AxisSurfaces(typing.NamedTuple):
@@ -19,8 +25,8 @@ class AxisPos(typing.NamedTuple):
 
 
 class AxisGrids(typing.NamedTuple):
-    x_axis_grid: list[pygame.rect.Rect]
-    y_axis_grid: list[pygame.rect.Rect]
+    x_axis: list[pygame.rect.Rect]
+    y_axis: list[pygame.rect.Rect]
 
 
 class AxisRects(typing.NamedTuple):
@@ -29,6 +35,10 @@ class AxisRects(typing.NamedTuple):
 
 
 class BoardAxisGui:
+
+    @staticmethod
+    def get_font() -> pygame.font.Font:
+        return pygame.font.Font(FONT_FILE, int(GameSize.get_relative_size(AXIS_FONT_SIZE)))
 
     @staticmethod
     def calculate_axis_rects() -> AxisRects:
@@ -48,7 +58,7 @@ class BoardAxisGui:
         return AxisSurfaces(x_axis_surface, y_axis_surface)
 
     @staticmethod
-    def create_grids() -> AxisGrids:
+    def create_axis() -> AxisGrids:
         x_axis_grid: list[pygame.rect.Rect] = []
         current_pos: pygame.math.Vector2 = pygame.math.Vector2(0)
         size = SQUARE_SIZE * GameSize.get_scale(), SQUARE_SIZE * GameSize.get_scale()
@@ -64,13 +74,25 @@ class BoardAxisGui:
 
         return AxisGrids(x_axis_grid, y_axis_grid)
 
+    @staticmethod
+    def create_values_dict() -> dict[str, pygame.surface.Surface]:
+        values_dict: dict[str, pygame.surface.Surface] = {}
+        anti_alias: bool = False
+        font = BoardAxisGui.get_font()
+        for rank, file in enumerate(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']):
+            values_dict[str(rank + 1)] = font.render(str(rank + 1), anti_alias, AssetManager.get_theme().light_color)
+            values_dict[file] = font.render(file, anti_alias, AssetManager.get_theme().light_color)
+        return values_dict
+
     def __init__(self, board_pos: pygame.rect.Rect, side: Side) -> None:
         self.board_pos: pygame.rect.Rect = board_pos
         self.side: Side = side
         self.axis_surfaces: AxisSurfaces = BoardAxisGui.create_surfaces()
-        self.axis_grids: AxisGrids = BoardAxisGui.create_grids()
+        self.axis: AxisGrids = BoardAxisGui.create_axis()
         self.axis_pos: AxisPos = self.calculate_pos()
-        self.render_values()
+        self.values_render_dict: dict[str, pygame.surface.Surface] = BoardAxisGui.create_values_dict()
+        self.update_surfaces()
+        self.prev_hover: TileHover | None = None
 
     def calculate_pos(self) -> AxisPos:
         x_axis_rect = self.axis_surfaces.x_axis_surface.get_rect()
@@ -81,22 +103,54 @@ class BoardAxisGui:
         y_axis_rect.y += BOARD_OUTLINE_THICKNESS
         return AxisPos(x_axis_rect, y_axis_rect)
 
-    def render_values(self) -> None:
-        anti_alias: bool = False
-        font_color: tuple[int, int, int] = 255, 255, 255
-        font = pygame.font.Font(FONT_FILE, int(GameSize.get_relative_size(AXIS_FONT_SIZE)))
+    def update_surfaces(self) -> None:
         letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
         if self.side is Side.BLACK: letters = letters[::-1]
-        for rect, letter in zip(self.axis_grids.x_axis_grid, letters):
-            text_render = font.render(letter, anti_alias, font_color)
+        for rect, file in zip(self.axis.x_axis, letters):
+            file_render = self.values_render_dict[file]
             self.axis_surfaces.x_axis_surface.blit(
-                text_render, text_render.get_rect(centerx=rect.centerx))
+                file_render, file_render.get_rect(centerx=rect.centerx))
 
-        for index, rect in enumerate(self.axis_grids.y_axis_grid):
+        for index, rect in enumerate(self.axis.y_axis):
             value = str(index + 1) if self.side is Side.BLACK else str(BOARD_SIZE - index)
-            text_render = font.render(value, anti_alias, font_color)
+            rank_render = self.values_render_dict[value]
             self.axis_surfaces.y_axis_surface.blit(
-                text_render, text_render.get_rect(centery=rect.centery))
+                rank_render, rank_render.get_rect(centery=rect.centery))
+
+    def update_axis_val_render(self, tile_hover: TileHover | None, render_color: tuple[int, int, int]) -> None:
+        if tile_hover is None:
+            self.reset_hover()
+        self.values_render_dict[tile_hover.rank] = BoardAxisGui.get_font().render(
+            tile_hover.rank,
+            False,
+            render_color
+        )
+        self.values_render_dict[tile_hover.file] = BoardAxisGui.get_font().render(
+            tile_hover.file,
+            False,
+            render_color
+        )
+        self.update_surfaces()
+
+    def reset_hover(self) -> None:
+        self.values_render_dict = BoardAxisGui.create_values_dict()
+        self.update_surfaces()
+
+    def update_hover_highlight(self, tile: BoardTile | None) -> None:
+        if tile is None:
+            if self.prev_hover is not None:
+                self.prev_hover = None
+                self.reset_hover()
+            return
+        current_hover: TileHover = TileHover(*tile.algebraic_notation.coordinates)
+        if self.prev_hover is None:
+            self.prev_hover = current_hover
+        else:
+            if self.prev_hover == current_hover:
+                return
+        self.update_axis_val_render(self.prev_hover, AssetManager.get_theme().light_color)
+        self.update_axis_val_render(current_hover, VERIFY_FONT_COLOR)
+        self.prev_hover = current_hover
 
     def render(self) -> None:
         GameSurface.get().blit(self.axis_surfaces.x_axis_surface, self.axis_pos.x_axis_pos)
