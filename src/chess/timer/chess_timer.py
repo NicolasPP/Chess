9facc0608748d4941f808319f4055c0eae715c4f
Nicolas <nicolas.parucker@hotmail.com>
2023-04-19 05:18:22 +0200

    moved TimerConfig and DefaultConfigs to its own script so server does not need pygame to run.
    moved both chess_timer.py and timer_config.py to chess.timer

--- src/chess/timer/chess_timer.py
+++ src/chess/timer/chess_timer.py
@@ -1,33 +1,7 @@
-import dataclasses
-
 import pygame
 
 from chess.asset.asset_manager import AssetManager
 from chess.game.game_surface import GameSurface
-
-
-@dataclasses.dataclass
-class TimerConfig:
-    # seconds
-    time: float
-    increment: float
-
-
-class DefaultConfigs:
-    # Bullet time controls refer to any time control that is faster than three minutes per player.
-    BULLET_1 = TimerConfig(1 * 60, 0)
-    BULLET_1_1 = TimerConfig(1 * 60, 1)
-    BULLET_2_1 = TimerConfig(2 * 60, 1)
-
-    # Blitz time controls are between three and ten minutes per player.
-    BLITZ_3 = TimerConfig(3 * 60, 0)
-    BLITZ_3_2 = TimerConfig(3 * 60, 2)
-    BLITZ_5 = TimerConfig(5 * 60, 0)
-
-    # Rapid time controls are any time controls that are longer than ten minutes per player.
-    RAPID_15_10 = TimerConfig(15 * 60, 10)
-    RAPID_30 = TimerConfig(30 * 60, 0)
-    RAPID_60 = TimerConfig(60 * 60, 0)
 
 
 class ChessTimer:
