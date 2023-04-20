b4df5e119b46b8f35bda94cb42f8be2cdcafdca3
Nicolas <nicolas.parucker@hotmail.com>
2023-04-20 13:02:04 +0200

    testing generate_move_text

--- tests/test_with_played_games.py
+++ tests/test_with_played_games.py
@@ -2,19 +2,32 @@ import pytest
 
 from chess.movement.validate_move import is_move_valid
 from chess.notation.forsyth_edwards_notation import Fen
-import chess.notation.portable_game_notation as game_notation
+from chess.notation.portable_game_notation import PortableGameNotation, get_an_from_pgn_game, Game, generate_move_text
 
 
-def get_games() -> list[game_notation.Game]:
-    magnus = game_notation.PortableGameNotation('game_data/magnus_carlsen_latest_games.pgn')
-    hikaru = game_notation.PortableGameNotation('game_data/hikaru_nakamura_latest_games.pgn')
+def get_games() -> list[Game]:
+    magnus = PortableGameNotation('game_data/magnus_carlsen_latest_games.pgn')
+    hikaru = PortableGameNotation('game_data/hikaru_nakamura_latest_games.pgn')
     return magnus.games + hikaru.games
 
 
 @pytest.mark.played_games
 @pytest.mark.parametrize("game", get_games())
-def test_against_played_games(game: game_notation.Game):
+def test_against_played_games(game: Game):
     fen = Fen()
-    for from_an, dest_an, target_fen in game_notation.get_an_from_pgn_game(game):
-        assert is_move_valid(from_an.index, dest_an.index, fen)
-        fen.make_move(from_an.index, dest_an.index, target_fen)
+    for reg_move, pgn_move in zip(get_an_from_pgn_game(game), game.pgn_moves):
+        white_reg = reg_move.white_move
+        black_reg = reg_move.black_move
+        if reg_move.white_move is not None:
+            white_pgn = generate_move_text(fen, white_reg.from_an, white_reg.dest_an, white_reg.target_fen)
+            assert pgn_move.white_move == white_pgn, f' {pgn_move.white_move}  {white_pgn}'
+            assert_valid_move(fen, white_reg.from_an, white_reg.dest_an, white_reg.target_fen)
+        if reg_move.black_move is not None:
+            black_pgn = generate_move_text(fen, black_reg.from_an, black_reg.dest_an, black_reg.target_fen)
+            assert pgn_move.black_move == black_pgn, f' {pgn_move.black_move}  {black_pgn}'
+            assert_valid_move(fen, black_reg.from_an, black_reg.dest_an, black_reg.target_fen)
+
+
+def assert_valid_move(fen: Fen, from_an, dest_an, target_fen) -> None:
+    assert is_move_valid(from_an.index, dest_an.index, fen)
+    fen.make_move(from_an.index, dest_an.index, target_fen)
