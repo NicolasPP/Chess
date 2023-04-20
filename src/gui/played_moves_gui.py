582e2e60e1ad27c8136693cd77ecd84d72bcb6b7
Nicolas <nicolas.parucker@hotmail.com>
2023-04-20 13:02:56 +0200

    keeping track of what moves have been played, moves are turned into PGN moves

---
+++ src/gui/played_moves_gui.py
@@ -0,0 +1,15 @@
+from chess.notation.algebraic_notation import AlgebraicNotation
+from chess.notation.portable_game_notation import generate_move_text
+from chess.notation.forsyth_edwards_notation import Fen
+
+
+class PlayedMovesGui:
+    def __init__(self) -> None:
+        self.played_moves: list[str] = []
+
+    def add_played_move(self, from_index: int, dest_index: int, fen: Fen, target_fen: str, show=False) -> None:
+        from_an = AlgebraicNotation.get_an_from_index(from_index)
+        dest_an = AlgebraicNotation.get_an_from_index(dest_index)
+        move = generate_move_text(fen, from_an, dest_an, target_fen)
+        print(move)
+        self.played_moves.append(move)
