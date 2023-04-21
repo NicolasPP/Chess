14c6fe8bf90b2214924c0ca8fea87c1cc1efdb32
Nicolas <nicolas.parucker@hotmail.com>
2023-04-21 09:12:58 +0200

    remove unused function parameter

--- src/gui/played_moves_gui.py
+++ src/gui/played_moves_gui.py
@@ -7,7 +7,7 @@ class PlayedMovesGui:
     def __init__(self) -> None:
         self.played_moves: list[str] = []
 
-    def add_played_move(self, from_index: int, dest_index: int, fen: Fen, target_fen: str, show=False) -> None:
+    def add_played_move(self, from_index: int, dest_index: int, fen: Fen, target_fen: str) -> None:
         from_an = AlgebraicNotation.get_an_from_index(from_index)
         dest_an = AlgebraicNotation.get_an_from_index(dest_index)
         move = generate_move_text(fen, from_an, dest_an, target_fen)
