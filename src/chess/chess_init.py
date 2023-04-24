6b7ab60574c609f606797a1768d7e48b52d8a65c
Nicolas <nicolas.parucker@hotmail.com>
2023-04-24 09:30:06 +0200

    adding played games background surface to game size width

--- src/chess/chess_init.py
+++ src/chess/chess_init.py
@@ -4,10 +4,11 @@ from chess.movement.piece_movement import PieceMovement
 from chess.asset.chess_assets import ChessTheme, PieceSetAsset, PieceSetAssets
 from chess.asset.asset_manager import AssetManager
 from gui.board_axis_gui import BoardAxisGui, AxisRects
-from gui.end_game_gui import EndGameGui, EndGameRects
+from gui.end_game_gui import EndGameGui
 from chess.game.game_surface import GameSurface
 from chess.game.game_size import GameSize
 from gui.timer_gui import TimerGui, TimerRects
+from gui.played_moves_gui import PlayedMovesGui, PlayedMovesRects
 from chess.board.chess_board import Board
 from chess.notation.forsyth_edwards_notation import FenChars
 from config import GAME_SURFACE_SPACING
@@ -20,33 +21,22 @@ def init_chess(theme: ChessTheme, piece_set: PieceSetAsset, scale: float) -> Non
     GameSize.load_scale(scale)
 
     board_rect: pygame.rect.Rect = Board.calculate_board_rect()
+    end_game_rect: pygame.rect.Rect = EndGameGui.calculate_end_game_rect()
     timer_rects: TimerRects = TimerGui.calculate_timer_rects()
     axis_rects: AxisRects = BoardAxisGui.calculate_axis_rects()
-    end_game_rects: EndGameRects = EndGameGui.calculate_end_game_rects()
+    played_moves_rects: PlayedMovesRects = PlayedMovesGui.calculate_played_moves_rects()
     '''
     -- width --
-    y_axis
-    board
-    max_width(resign, draw)
+    y_axis board max_width(end_game, played_moves_background)
     '''
-    max_rect = end_game_rects.resign_rect \
-        if end_game_rects.resign_rect.width >= end_game_rects.draw_rect.width else end_game_rects.draw_rect
+    max_rect = max(end_game_rect, played_moves_rects.background, key=lambda rect: rect.width)
     GameSize.add_rects_width(axis_rects.y_axis, board_rect, max_rect)
 
     '''
        -- height --
-       timer
-       timer_spacing
-       board
-       x_axis
-       timer
+       timer timer_spacing board x_axis timer
     '''
-    GameSize.add_rects_height(
-        timer_rects.timer,
-        timer_rects.spacing,
-        board_rect,
-        axis_rects.x_axis,
-        timer_rects.timer)
+    GameSize.add_rects_height(timer_rects.timer, timer_rects.spacing, board_rect, axis_rects.x_axis, timer_rects.timer)
 
     GameSurface.create_surface()
     pygame.display.set_mode(calculate_window_size(*GameSurface.get().get_size()))
@@ -62,5 +52,4 @@ def init_chess(theme: ChessTheme, piece_set: PieceSetAsset, scale: float) -> Non
 
 
 def calculate_window_size(width: int, height: int) -> tuple[int, int]:
-    bigger_side = max(width, height)
-    return bigger_side + GAME_SURFACE_SPACING, bigger_side + GAME_SURFACE_SPACING
+    return width + GAME_SURFACE_SPACING, height + GAME_SURFACE_SPACING
