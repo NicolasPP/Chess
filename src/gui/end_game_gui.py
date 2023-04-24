a4044bf909128a26312b4b2a23499532abf2418d
Nicolas <nicolas.parucker@hotmail.com>
2023-04-24 09:32:41 +0200

    removed EndGameRects because resign and draw buttons were using the same rect
    EndGameGui.calculate_end_game_rect now returns a pygame rect

--- src/gui/end_game_gui.py
+++ src/gui/end_game_gui.py
@@ -10,30 +10,19 @@ from chess.game.game_size import GameSize
 from config import *
 
 
-class EndGameRects(typing.NamedTuple):
-    resign_rect: pygame.rect.Rect
-    draw_rect: pygame.rect.Rect
-
-
 class EndGameGui:
     @staticmethod
-    def calculate_end_game_rects() -> EndGameRects:
+    def calculate_end_game_rect() -> pygame.rect.Rect:
         default_pos: tuple[int, int] = 0, 0
-        resign_rect = pygame.rect.Rect(
+        return pygame.rect.Rect(
             default_pos,
-            (GameSize.get_relative_size(RESIGN_BUTTON_WIDTH), GameSize.get_relative_size(RESIGN_BUTTON_HEIGHT)))
-        draw_rect = pygame.rect.Rect(
-            default_pos,
-            (GameSize.get_relative_size(DRAW_BUTTON_WIDTH), GameSize.get_relative_size(DRAW_BUTTON_HEIGHT)))
-        return EndGameRects(resign_rect, draw_rect)
+            (SQUARE_SIZE * GameSize.get_scale() * 2, SQUARE_SIZE * GameSize.get_scale() * 1/2))
 
     def __init__(self, board_rect: pygame.rect.Rect):
         self.board_rect: pygame.rect.Rect = board_rect
-        rects: EndGameRects = EndGameGui.calculate_end_game_rects()
-        self.offer_draw: ButtonGui = ButtonGui(
-            rects.draw_rect.width, rects.draw_rect.height, AssetManager.get_theme().dark_color)
-        self.resign: ButtonGui = ButtonGui(
-            rects.resign_rect.width, rects.resign_rect.height, AssetManager.get_theme().dark_color)
+        rect: pygame.rect.Rect = EndGameGui.calculate_end_game_rect()
+        self.offer_draw: ButtonGui = ButtonGui(rect.width, rect.height, AssetManager.get_theme().dark_color)
+        self.resign: ButtonGui = ButtonGui(rect.width, rect.height, AssetManager.get_theme().dark_color)
         self.game_over_gui: GameOverGui = GameOverGui(AssetManager.get_theme().dark_color)
         self.button_init()
         self.recalculate_pos()
@@ -47,6 +36,8 @@ class EndGameGui:
             FONT_FILE,
             int(GameSize.get_relative_size(RESIGN_FONT_SIZE)), False, AssetManager.get_theme().light_color
         )
+        self.offer_draw.set_hover_color(AssetManager.get_theme().light_color)
+        self.resign.set_hover_color(AssetManager.get_theme().light_color)
         self.offer_draw.set_label(OFFER_DRAW_LABEL)
         self.resign.set_label(RESIGN_LABEL)
 
@@ -58,7 +49,7 @@ class EndGameGui:
         self.offer_draw.rect.bottomleft = self.board_rect.bottomright
         self.resign.rect.bottomleft = self.board_rect.bottomright
 
-        self.offer_draw.rect.x += END_GAME_GUI_SPACING
-        self.resign.rect.x += END_GAME_GUI_SPACING
+        self.offer_draw.rect.x += GUI_SPACING
+        self.resign.rect.x += GUI_SPACING
 
-        self.resign.rect.y -= (self.offer_draw.rect.height + END_GAME_GUI_SPACING)
+        self.resign.rect.y -= (self.offer_draw.rect.height + GUI_SPACING)
