9d8006eea62974243b90f29ebcf3cb8fc492faed
Nicolas <nicolas.parucker@hotmail.com>
2023-05-07 16:46:07 +0200

    added abstraction required for managing switching pages in the launcher

--- src/launcher/tk/tk_launcher.py
+++ src/launcher/tk/tk_launcher.py
@@ -1,155 +1,44 @@
-# import tkinter as tk
-# from chess_launcher import ChessLauncher
-# from tkinter import ttk
-# from config import *
-#
-# window = tk.Tk()
-# window.resizable(False, False)
-# window.title('chess launcher')
-# window.geometry(WINDOW_SIZE)
-# chess_launcher: ChessLauncher = ChessLauncher()
-#
-# single_player_button = ttk.Button(master=window, text='single player', command=chess_launcher.launch_single_player)
-#
-# single_player_button.pack()
-#
-# window.mainloop()
-
-
 import tkinter as tk
-import ttkbootstrap as ttk
 
-LARGEFONT = ("Verdana", 35)
+from launcher.tk.page_frame import PageFrame
+from launcher.tk.pages.offline_page import OfflinePage
+from launcher.tk.pages.online_page import OnlinePage
+from launcher.tk.pages.server_page import ServerPage
+from launcher.tk.pages.settings_page import SettingsPage
+from launcher.tk.pages.start_page import StartPage
+
+
+class PageManager:
+    def __init__(self) -> None:
+        self.pages: dict[str, PageFrame] = {}
+
+    def add_page(self, page_frame: PageFrame) -> None:
+        self.pages[type(page_frame).__name__] = page_frame
+
+    def get_page(self, page_name: str) -> PageFrame:
+        page_frame = self.pages.get(page_name)
+        if page_frame is None:
+            raise Exception(f'page name: {page_name} is not a valid id')
+        return page_frame
 
 
 class ChessTkinterLauncher(tk.Tk):
+
     def __init__(self, *args, **kwargs) -> None:
         tk.Tk.__init__(self, *args, **kwargs)
-        self.root_container: tk.Frame = tk.Frame(self)
-        self.root_container.pack(side="top", fill="both", expand=True)
+        self.root_frame: tk.Frame = tk.Frame(self, height=100, width=150, bg="Red")
+        self.root_frame.pack(side="top", fill="none")
+        self.page_manager: PageManager = PageManager()
+        self.page_manager.add_page(StartPage(self.root_frame, self))
+        self.page_manager.add_page(OfflinePage(self.root_frame, self))
+        self.page_manager.add_page(OnlinePage(self.root_frame, self))
+        self.page_manager.add_page(SettingsPage(self.root_frame, self))
+        self.page_manager.add_page(ServerPage(self.root_frame, self))
+        self.show_page(StartPage.__name__)
+
+    def show_page(self, page_name: str) -> None:
+        self.page_manager.get_page(page_name).tkraise()
 
 
-
-class tkinterApp(tk.Tk):
-
-    # __init__ function for class tkinterApp
-    def __init__(self, *args, **kwargs):
-        # __init__ function for class Tk
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        # creating a container
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        # initializing frames to an empty array
-        self.frames = {}
-
-        # iterating through a tuple consisting
-        # of the different page layouts
-        for F in (StartPage, Page1, Page2):
-            frame = F(container, self)
-
-            # initializing frame of that object from
-            # startpage, page1, page2 respectively with
-            # for loop
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    # to display the current frame passed as
-    # parameter
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-
-
-# first window frame startpage
-
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        # label of frame Layout 2
-        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
-
-        # putting the grid in its place by using
-        # grid
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 2 with text layout2
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-# second window frame page1
-class Page1(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 1", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text="StartPage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place
-        # by using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button2 = ttk.Button(self, text="Page 2",
-                             command=lambda: controller.show_frame(Page2))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-# third window frame page2
-class Page2(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
-
-        # button to show frame 2 with text
-        # layout2
-        button1 = ttk.Button(self, text="Page 1",
-                             command=lambda: controller.show_frame(Page1))
-
-        # putting the button in its place by
-        # using grid
-        button1.grid(row=1, column=1, padx=10, pady=10)
-
-        # button to show frame 3 with text
-        # layout3
-        button2 = ttk.Button(self, text="Startpage",
-                             command=lambda: controller.show_frame(StartPage))
-
-        # putting the button in its place by
-        # using grid
-        button2.grid(row=2, column=1, padx=10, pady=10)
-
-
-# Driver Code
-app = tkinterApp()
-app.mainloop()
+if __name__ == "__main__":
+    ChessTkinterLauncher().mainloop()
