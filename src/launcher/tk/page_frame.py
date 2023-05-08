5b3a756f0d221ac79d4f01a38b63ff22f24a8f0c
Nicolas <nicolaspiolaparucker@gmail.com>
2023-05-08 19:46:07 +0700

    changing style and height of PageFrame in __init__

--- src/launcher/tk/page_frame.py
+++ src/launcher/tk/page_frame.py
@@ -1,9 +1,10 @@
-import tkinter as tk
+import ttkbootstrap as ttk
 
 
-class PageFrame(tk.Frame):
-    def __init__(self, parent_frame: tk.Frame, launcher: tk.Tk) -> None:
-        tk.Frame.__init__(self, parent_frame)
-        self.parent_frame: tk.Frame = parent_frame
-        self.launcher: tk.Tk = launcher
+class PageFrame(ttk.Frame):
+    def __init__(self, parent_frame: ttk.Frame) -> None:
+        ttk.Frame.__init__(self, parent_frame)
+        self["style"] = "page_frame.TFrame"
+        self["width"], self["height"] = parent_frame["width"], parent_frame["height"]
+        self.parent_frame: ttk.Frame = parent_frame
         self.grid(column=0, row=0, sticky='nsew')
