6272e9aaa3fe8d81a53ced9235f4f29309e113db
Nicolas <nicolas.parucker@hotmail.com>
2023-05-09 20:07:06 +0200

    fixed multithreading issue, where performance was being affected.

--- src/chess/bot/chess_bot_stockfish.py
+++ src/chess/bot/chess_bot_stockfish.py
@@ -1,6 +1,5 @@
 import datetime
-
-from _thread import start_new_thread
+import threading
 from stockfish import Stockfish
 
 from chess.notation.forsyth_edwards_notation import Fen
@@ -11,28 +10,33 @@ from chess.board.side import Side
 
 
 class StockFishBot:
-    def __init__(self, game_fen: Fen, player: Player, side: Side) -> None:
+    def __init__(self, fen: Fen, side: Side) -> None:
         self.side: Side = side
-        self.stockfish: Stockfish = Stockfish()
-        start_new_thread(self.play_game, (player, game_fen))
+        self.fen: Fen = fen
+        self.stock_fish: Stockfish = Stockfish(
+            path=r"C:\Users\nicol\Downloads\stockfish_15.1_win_x64_popcnt\stockfish_15.1_win_x64_popcnt\stockfish-windows-2022-x86-64-modern.exe")
+        self.move_thread: threading.Thread = self.get_move_thread()
 
     def get_best_move(self, game_fen: Fen) -> str:
-        self.stockfish.is_fen_valid(game_fen.notation)
-        self.stockfish.set_fen_position(game_fen.notation)
-        return self.stockfish.get_best_move()
+        self.stock_fish.is_fen_valid(game_fen.notation)
+        self.stock_fish.set_fen_position(game_fen.notation)
+        return self.stock_fish.get_best_move()
 
-    def make_move(self, game_fen: Fen, side: Side | None = None) -> None:
+    def get_move_thread(self) -> threading.Thread:
+        return threading.Thread(target=self.make_move)
+
+    def make_move(self, side: Side | None = None) -> None:
         if side is None: side = self.side
-        move = self.get_best_move(game_fen)
+        move = self.get_best_move(self.fen)
         time_iso = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
         from_an_val, dest_an_val = move[:2], move[2:]
         from_an = AlgebraicNotation(*from_an_val)
         dest_an = AlgebraicNotation(*dest_an_val)
 
-        ks_king_index = 62 if game_fen.is_white_turn() else 6
-        qs_king_index = 58 if game_fen.is_white_turn() else 2
-        king_index = 60 if game_fen.is_white_turn() else 4
+        ks_king_index = 62 if self.fen.is_white_turn() else 6
+        qs_king_index = 58 if self.fen.is_white_turn() else 2
+        king_index = 60 if self.fen.is_white_turn() else 4
 
         if from_an.index == king_index:
             if dest_an.index == ks_king_index:
@@ -44,15 +48,15 @@ class StockFishBot:
             CommandManager.from_coordinates: from_an.coordinates,
             CommandManager.dest_coordinates: dest_an.coordinates,
             CommandManager.side: side.name,
-            CommandManager.target_fen: game_fen[from_an.index],
+            CommandManager.target_fen: self.fen[from_an.index],
             CommandManager.time_iso: time_iso
         }
 
         move_command = CommandManager.get(ClientCommand.MOVE, move_info)
         send_command(True, None, move_command)
 
-    def play_game(self, player: Player, game_fen: Fen) -> None:
-        while not player.game_over:
-            is_turn: bool = not game_fen.is_white_turn() if self.side == Side.BLACK else game_fen.is_white_turn()
-            if is_turn:
-                self.make_move(game_fen)
+    def play_game(self, player: Player) -> None:
+        if not player.turn and not self.move_thread.is_alive():
+            new_thread: threading.Thread = self.get_move_thread()
+            new_thread.start()
+            self.move_thread = new_thread
