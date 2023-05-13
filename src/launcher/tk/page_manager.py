0738b541ec205b7fef0dfb3875490e5f167e9b7a
Nicolas <nicolas.parucker@hotmail.com>
2023-05-13 22:52:07 +0200

    PageManger now will show the page by unpacking the current PageFrame and packing the requested one

--- src/launcher/tk/page_manager.py
+++ src/launcher/tk/page_manager.py
@@ -4,6 +4,7 @@ from launcher.tk.page_frame import PageFrame
 class PageManager:
     def __init__(self) -> None:
         self.pages: dict[str, PageFrame] = {}
+        self.current_page: PageFrame | None = None
 
     def add_page(self, page_frame: PageFrame) -> None:
         self.pages[type(page_frame).__name__] = page_frame
@@ -14,5 +15,13 @@ class PageManager:
             raise Exception(f'page name: {page_name} is not a valid id')
         return page_frame
 
+    def set_current_page(self, page: PageFrame) -> None:
+        self.current_page = page
+
     def show_page(self, page_name: str) -> None:
-        self.get_page(page_name).tkraise()
+        if self.current_page is not None:
+            self.current_page.pack_forget()
+
+        page: PageFrame = self.get_page(page_name)
+        self.set_current_page(page)
+        page.pack(fill="both", expand=True)
