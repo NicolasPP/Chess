02996471ad55ef20c1ad567511a000a08239fe77
Nicolas <nicolas.parucker@hotmail.com>
2023-05-13 22:51:07 +0200

    no longer packing in PageFrame __init__ function

--- src/launcher/tk/page_frame.py
+++ src/launcher/tk/page_frame.py
@@ -3,8 +3,6 @@ import ttkbootstrap as ttk
 
 class PageFrame(ttk.Frame):
     def __init__(self, parent_frame: ttk.Frame) -> None:
-        ttk.Frame.__init__(self, parent_frame, bootstyle="dark")
+        ttk.Frame.__init__(self, parent_frame)
         self["width"], self["height"] = parent_frame["width"], parent_frame["height"]
         self.parent_frame: ttk.Frame = parent_frame
-        self.grid(column=0, row=0, sticky='nsew')
-        self.pack_propagate(False)
