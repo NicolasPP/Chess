2614a65b93f56ba54866f4ac013ae8a592e3be90
Nicolas <nicolas.parucker@hotmail.com>
2023-05-29 12:55:37 +0200

    made create_settings_widgets into class variable

--- src/launcher/tk/components/settings_comp.py
+++ src/launcher/tk/components/settings_comp.py
@@ -20,56 +20,12 @@ class SettingsWidgets(typing.NamedTuple):
 
 
 class SettingsComponent(Component):
-    # FIXME make this class variable
-    @staticmethod
-    def create_settings_widgets(settings_frame: ttk.LabelFrame, path_entry_str: ttk.StringVar) -> SettingsWidgets:
-        engine_valid: ttk.Label = ttk.Label(settings_frame, text="Stock Fish ready!", foreground='green')
-        path_entry: ttk.Entry = ttk.Entry(settings_frame, textvariable=path_entry_str, width=18)
-        path_entry.bind("<Button>", lambda e: path_entry_str.set(''))
-        create_bot: ttk.Button = ttk.Button(settings_frame, text="create bot")
-
-        theme_label: ttk.Label = ttk.Label(settings_frame, text="Theme: ")
-        theme_id_name: str = str(UserConfig.get().config.data.theme_id)
-        if theme_id_name == "-1": theme_id_name = 'RANDOM'
-        theme_options: ttk.Menubutton = ttk.Menubutton(settings_frame, text=theme_id_name)
-        theme_menu: ttk.Menu = ttk.Menu(theme_options)
-        theme_menu.add_radiobutton(value="1", label="1", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=1))
-        theme_menu.add_radiobutton(value="2", label="2", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=2))
-        theme_menu.add_radiobutton(value="3", label="3", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=3))
-        theme_menu.add_radiobutton(value="4", label="4", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=4))
-        theme_menu.add_radiobutton(value="RANDOM", label="RANDOM", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=-1))
-        theme_options['menu'] = theme_menu
-
-        asset_label: ttk.Label = ttk.Label(settings_frame, text="Asset: ")
-        asset_options: ttk.Menubutton = ttk.Menubutton(settings_frame, text=UserConfig.get().data.asset_name)
-        asset_menu: ttk.Menu = ttk.Menu(asset_options)
-        asset_menu.add_radiobutton(value="SMALL", label="SMALL", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(asset_options,
-                                                                             asset_name="SMALL"))
-        asset_menu.add_radiobutton(value="LARGE", label="LARGE", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(asset_options, asset_name="LARGE"))
-        asset_menu.add_radiobutton(value="RANDOM", label="RANDOM", background=BG_DARK, foreground=FG_DARK,
-                                   command=lambda: configure_pygame_launcher(asset_options, asset_name="RANDOM"))
-        asset_options['menu'] = asset_menu
-
-        current_scale: float = UserConfig.get().data.scale
-        size_scale_label: ttk.Label = ttk.Label(settings_frame, text=f"size : {current_scale}", anchor=ttk.W,
-                                                width=SCALE_LABEL_WIDTH)
-        size_scale: ttk.Scale = ttk.Scale(settings_frame, from_=3, to=7, style='warning', value=current_scale,
-                                          command=lambda size: handle_scale_click(size, size_scale_label))
-        return SettingsWidgets(engine_valid, path_entry, create_bot, theme_options, theme_label, asset_options,
-                               asset_label, size_scale, size_scale_label)
 
     def __init__(self, parent: ttk.Frame, is_bot_valid: ttk.BooleanVar) -> None:
-        super().__init__(parent, text="Settings")
+        super().__init__(parent, "Settings")
 
         path_entry_str: ttk.StringVar = ttk.StringVar(value="stock fish engine path")
-        settings_widgets = SettingsComponent.create_settings_widgets(self.frame, path_entry_str)
+        settings_widgets = self.create_settings_widgets(path_entry_str)
         settings_widgets.create_bot[ttk.COMMAND] = lambda: create_bot_command(settings_widgets, is_bot_valid,
                                                                               path_entry_str)
 
@@ -97,6 +53,49 @@ class SettingsComponent(Component):
         else:
             settings_widgets.path_entry.grid(row=3, column=0, sticky=ttk.NSEW, padx=SETTINGS_PAD, pady=SETTINGS_PAD)
             settings_widgets.create_bot.grid(row=3, column=1, sticky=ttk.NSEW, padx=SETTINGS_PAD, pady=SETTINGS_PAD)
+
+    def create_settings_widgets(self, path_entry_str: ttk.StringVar) -> SettingsWidgets:
+        engine_valid: ttk.Label = ttk.Label(self.frame, text="Stock Fish ready!", foreground='green')
+        path_entry: ttk.Entry = ttk.Entry(self.frame, textvariable=path_entry_str, width=18)
+        path_entry.bind("<Button>", lambda e: path_entry_str.set(''))
+        create_bot: ttk.Button = ttk.Button(self.frame, text="create bot")
+
+        theme_label: ttk.Label = ttk.Label(self.frame, text="Theme: ")
+        theme_id_name: str = str(UserConfig.get().config.data.theme_id)
+        if theme_id_name == "-1": theme_id_name = 'RANDOM'
+        theme_options: ttk.Menubutton = ttk.Menubutton(self.frame, text=theme_id_name)
+        theme_menu: ttk.Menu = ttk.Menu(theme_options)
+        theme_menu.add_radiobutton(value="1", label="1", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=1))
+        theme_menu.add_radiobutton(value="2", label="2", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=2))
+        theme_menu.add_radiobutton(value="3", label="3", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=3))
+        theme_menu.add_radiobutton(value="4", label="4", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=4))
+        theme_menu.add_radiobutton(value="RANDOM", label="RANDOM", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(theme_options, theme_id=-1))
+        theme_options['menu'] = theme_menu
+
+        asset_label: ttk.Label = ttk.Label(self.frame, text="Asset: ")
+        asset_options: ttk.Menubutton = ttk.Menubutton(self.frame, text=UserConfig.get().data.asset_name)
+        asset_menu: ttk.Menu = ttk.Menu(asset_options)
+        asset_menu.add_radiobutton(value="SMALL", label="SMALL", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(asset_options,
+                                                                             asset_name="SMALL"))
+        asset_menu.add_radiobutton(value="LARGE", label="LARGE", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(asset_options, asset_name="LARGE"))
+        asset_menu.add_radiobutton(value="RANDOM", label="RANDOM", background=BG_DARK, foreground=FG_DARK,
+                                   command=lambda: configure_pygame_launcher(asset_options, asset_name="RANDOM"))
+        asset_options['menu'] = asset_menu
+
+        current_scale: float = UserConfig.get().data.scale
+        size_scale_label: ttk.Label = ttk.Label(self.frame, text=f"size : {current_scale}", anchor=ttk.W,
+                                                width=SCALE_LABEL_WIDTH)
+        size_scale: ttk.Scale = ttk.Scale(self.frame, from_=3, to=7, style='warning', value=current_scale,
+                                          command=lambda size: handle_scale_click(size, size_scale_label))
+        return SettingsWidgets(engine_valid, path_entry, create_bot, theme_options, theme_label, asset_options,
+                               asset_label, size_scale, size_scale_label)
 
 
 def create_bot_command(settings_widgets: SettingsWidgets, is_bot_valid: ttk.BooleanVar,
