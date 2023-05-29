91805af27d55651c7446133824abf56f3dcf8f59
Nicolas <nicolas.parucker@hotmail.com>
2023-05-29 12:47:20 +0200

    implemented function to set the title of a component

--- src/launcher/tk/components/tk_component.py
+++ src/launcher/tk/components/tk_component.py
@@ -3,8 +3,16 @@ from ttkbootstrap import ttk
 
 class Component:
 
-    def __init__(self, parent: ttk.Frame, text: str) -> None:
-        self.frame: ttk.LabelFrame = ttk.LabelFrame(parent, text=text)
+    @staticmethod
+    def get_title(title: str) -> str:
+        return f" {title.strip()} "
+
+    def __init__(self, parent: ttk.Frame, title: str) -> None:
+        self.frame: ttk.LabelFrame = ttk.LabelFrame(parent)
+        self.set_title(title)
 
     def get_frame(self) -> ttk.LabelFrame:
         return self.frame
+
+    def set_title(self, title) -> None:
+        self.frame["text"] = Component.get_title(title)
