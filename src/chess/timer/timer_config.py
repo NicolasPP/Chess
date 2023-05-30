2e66096a6de57ee16858ce87b3d08834101f2283
Nicolas <nicolas.parucker@hotmail.com>
2023-05-30 13:51:03 +0200

    added name variable in timer config and fixed type hints

--- src/chess/timer/timer_config.py
+++ src/chess/timer/timer_config.py
@@ -6,29 +6,36 @@ class TimerConfig:
     # seconds
     time: float
     increment: float
+    name: str
+
+    def get_value_str(self) -> str:
+        return f"{self.time} {self.increment}"
+
+    def get_name(self) -> str:
+        return self.name
 
 
 class DefaultConfigs:
     # Bullet time controls refer to any time control that is faster than three minutes per player.
-    BULLET_1_0 = TimerConfig(1 * 60, 0)
-    BULLET_1_1 = TimerConfig(1 * 60, 1)
-    BULLET_2_1 = TimerConfig(2 * 60, 1)
+    BULLET_1_0: TimerConfig = TimerConfig(1 * 60, 0, "BULLET_1_0")
+    BULLET_1_1: TimerConfig = TimerConfig(1 * 60, 1, "BULLET_1_1")
+    BULLET_2_1: TimerConfig = TimerConfig(2 * 60, 1, "BULLET_2_1")
 
     # Blitz time controls are between three and ten minutes per player.
-    BLITZ_3_0 = TimerConfig(3 * 60, 0)
-    BLITZ_3_2 = TimerConfig(3 * 60, 2)
-    BLITZ_5_0 = TimerConfig(5 * 60, 0)
+    BLITZ_3_0: TimerConfig = TimerConfig(3 * 60, 0, "BLITZ_3_0")
+    BLITZ_3_2: TimerConfig = TimerConfig(3 * 60, 2, "BLITZ_3_2")
+    BLITZ_5_0: TimerConfig = TimerConfig(5 * 60, 0, "BLITZ_5_0")
 
     # Rapid time controls are any time controls that are longer than ten minutes per player.
-    RAPID_15_10 = TimerConfig(15 * 60, 10)
-    RAPID_30_0 = TimerConfig(30 * 60, 0)
-    RAPID_60_0 = TimerConfig(60 * 60, 0)
+    RAPID_15_10: TimerConfig = TimerConfig(15 * 60, 10, "RAPID_15_10")
+    RAPID_30_0: TimerConfig = TimerConfig(30 * 60, 0, "RAPID_30_0")
+    RAPID_60_0: TimerConfig = TimerConfig(60 * 60, 0, "RAPID_60_0")
 
     @staticmethod
     def get_timer_config(name: str) -> TimerConfig:
         if is_custom_name(name):
             time, increment = name.split()
-            return TimerConfig(float(time) * 60, float(increment))
+            return TimerConfig(float(time) * 60, float(increment), "CUSTOM")
 
         elif name == "BULLET_1_0":
             return DefaultConfigs.BULLET_1_0
