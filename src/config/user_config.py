6fa4c2a404ecdadf8fa9db1188b9c5f7013de5ce
Nicolas <nicolas.parucker@hotmail.com>
2023-05-31 06:05:10 +0200

    removed functions to get single players and multiplayer args

--- src/config/user_config.py
+++ src/config/user_config.py
@@ -3,9 +3,6 @@ import typing
 import dataclasses
 import pickle
 
-from chess.asset.chess_assets import PieceSetAssets, Themes, ChessTheme, PieceSetAsset
-from chess.timer.timer_config import TimerConfig, DefaultConfigs
-from chess.board.side import Side
 from config.tk_config import *
 
 PossibleConfigValues: typing.TypeAlias = str | float | int | bool
@@ -49,24 +46,6 @@ class UserConfig:
         pickle_user_data: bytes = pickle.dumps(self.data)
         with open(LOCAL_CONFIG_PATH, 'wb') as file:
             file.write(pickle_user_data)
-
-    def single_player_args(self) -> tuple[ChessTheme, float, PieceSetAsset, TimerConfig]:
-        theme: ChessTheme = Themes.get_theme(self.data.theme_id)
-        piece_set: PieceSetAsset = PieceSetAssets.get_asset(self.data.asset_name)
-        timer_config: TimerConfig = DefaultConfigs.get_timer_config(self.data.timer_config_name)
-        return theme, self.data.scale, piece_set, timer_config
-
-    def multi_player_args(self) -> tuple[str, ChessTheme, float, PieceSetAsset]:
-        theme: ChessTheme = Themes.get_theme(self.data.theme_id)
-        piece_set: PieceSetAsset = PieceSetAssets.get_asset(self.data.asset_name)
-        return self.data.server_ip, theme, self.data.scale, piece_set
-
-    def single_player_bot_args(self) -> tuple[ChessTheme, float, PieceSetAsset, TimerConfig, Side]:
-        theme: ChessTheme = Themes.get_theme(self.data.theme_id)
-        piece_set: PieceSetAsset = PieceSetAssets.get_asset(self.data.asset_name)
-        bot_side: Side = Side[self.data.bot_side_name]
-        timer_config: TimerConfig = DefaultConfigs.get_timer_config(self.data.timer_config_name)
-        return theme, self.data.scale, piece_set, timer_config, bot_side
 
     def update_config(self, update_config: bool = True, **new_values: PossibleConfigValues) -> None:
 
