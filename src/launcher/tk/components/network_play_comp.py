aeaad01d181aabd94f40d263bed51455df4f92e4
Nicolas <nicolas.parucker@hotmail.com>
2023-06-01 06:43:57 +0200

    small refactoring

--- src/launcher/tk/components/network_play_comp.py
+++ src/launcher/tk/components/network_play_comp.py
@@ -20,7 +20,7 @@ class NetworkPlayComponent(Component):
         offline_button: ttk.Button = ttk.Button(play_frame, text="Offline",
                                                 command=lambda: page_manger.show_page("OfflinePage"))
         is_logged_in: tk.BooleanVar = tk.BooleanVar(value=False)
-        is_logged_in.trace_add('write', lambda v, i, m: handle_update_online_button(online_button, is_logged_in))
+        is_logged_in.trace_add("write", lambda v, i, m: handle_update_online_button(online_button, is_logged_in))
         return NetworkPlayButtons(online_button, offline_button, is_logged_in)
 
     def __init__(self, parent: ttk.Frame, page_manager: PageManager) -> None:
@@ -35,4 +35,4 @@ class NetworkPlayComponent(Component):
 
 def handle_update_online_button(online_button: ttk.Button, is_logged_in: tk.BooleanVar) -> None:
     state = tk.NORMAL if is_logged_in.get() else tk.DISABLED
-    online_button["state"] = state
+    online_button.configure(state=state)
