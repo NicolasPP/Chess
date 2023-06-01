efd8ed5b7d2a2653fa0e4285f01343f0289cf24d
Nicolas <nicolas.parucker@hotmail.com>
2023-06-01 10:56:19 +0200

    you can choose if you want the logging to output to stdout rather than a file

--- src/chess/chess_logging.py
+++ src/chess/chess_logging.py
@@ -1,14 +1,34 @@
+import enum
 import logging
+import sys
+
 from config.pg_config import LOG_FORMAT, LOG_FILE_MODE
+
 formatter: logging.Formatter = logging.Formatter(LOG_FORMAT)
 
 
-def set_up_logging(name: str, log_file: str, log_level=logging.DEBUG) -> logging.Logger:
-    handler: logging.FileHandler = logging.FileHandler(log_file, mode=LOG_FILE_MODE)
-    handler.setFormatter(formatter)
+class LoggingOut(enum.Enum):
+    FILE = enum.auto()
+    STDOUT = enum.auto()
 
+
+def set_up_logging(name: str, out_type: LoggingOut, log_file: str | None = None, log_level=logging.DEBUG) -> \
+        logging.Logger:
+    handler: logging.FileHandler | logging.StreamHandler = get_handler(out_type, log_file)
+    handler.setFormatter(formatter)
     logger: logging.Logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logger.addHandler(handler)
-
     return logger
+
+
+def get_handler(out_type: LoggingOut, log_file: str | None = None) -> logging.FileHandler | logging.StreamHandler:
+    if out_type is LoggingOut.FILE:
+        assert log_file is not None, "if out type is FILE must provide file path"
+        return logging.FileHandler(log_file, mode=LOG_FILE_MODE)
+
+    elif out_type is LoggingOut.STDOUT:
+        return logging.StreamHandler(sys.stdout)
+
+    else:
+        raise Exception(f"logging out type : {out_type} is invalid")
