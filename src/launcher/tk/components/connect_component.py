5eb9530e3c0169acfc78b74c82d2b7a35537d027
Nicolas <nicolas.parucker@hotmail.com>
2023-06-03 20:56:26 +0200

    using the clientConnectResult to display error message when trying to connect

--- src/launcher/tk/components/connect_component.py
+++ src/launcher/tk/components/connect_component.py
@@ -4,15 +4,18 @@ from ttkbootstrap import ttk
 from launcher.tk.components.tk_component import Component
 from launcher.tk.page.page_manager import PageManager
 from launcher.tk.launcher_user import LauncherUser
+from chess.network.client.chess_client import ClientConnectResult
 
 
 class ConnectWidgets(typing.NamedTuple):
     server_ip_entry: ttk.Entry
+    error_label: ttk.Label
     connect_button: ttk.Button
 
 
 class ConnectVars(typing.NamedTuple):
     ip_entry_var: tk.StringVar
+    error_var: tk.StringVar
 
 
 class ConnectComponent(Component):
@@ -24,27 +27,30 @@ class ConnectComponent(Component):
         self.widgets: ConnectWidgets = self.create_widgets()
 
         self.widgets.server_ip_entry.pack(expand=True)
+        self.widgets.error_label.pack(expand=True)
         self.widgets.connect_button.pack(expand=True)
 
     def create_widgets(self) -> ConnectWidgets:
         server_ip_entry: ttk.Entry = ttk.Entry(self.frame, textvariable=self.vars.ip_entry_var)
+        error_label: ttk.Label = ttk.Label(self.frame, textvariable=self.vars.error_var, foreground="red")
         connect_button: ttk.Button = ttk.Button(self.frame, text="Connect", command=self.handle_connect)
-        return ConnectWidgets(server_ip_entry, connect_button)
+        return ConnectWidgets(server_ip_entry, error_label, connect_button)
 
     def handle_connect(self) -> None:
+        self.vars.error_var.set("")
+        self.widgets.error_label.update()
         server_ip: str = self.vars.ip_entry_var.get()
         if self.started_server.get() and not server_ip:
             server_ip = "127.0.0.1"
         LauncherUser.get_client().set_ip_address(server_ip)
-        is_connect_successful: bool = LauncherUser.get_client().start()
-
-        if is_connect_successful:
+        is_connect_successful: ClientConnectResult = LauncherUser.get_client().start()
+        if is_connect_successful.result:
             self.page_manager.show_page("ServerPage")
         else:
-            # TODO: add error label to show when the client cannot connect
-            pass
+            self.vars.error_var.set(is_connect_successful.error.strerror)
 
 
 def create_vars() -> ConnectVars:
     ip_entry_var: tk.StringVar = tk.StringVar()
-    return ConnectVars(ip_entry_var)
+    error_var: tk.StringVar = tk.StringVar()
+    return ConnectVars(ip_entry_var, error_var)
