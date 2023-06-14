b2b871011243865fda25b89f6f4477140088aed1
Nicolas <nicolaspiolaparucker@gmail.com>
2023-06-14 09:24:42 +0700

    handle exit unimplemented function

--- src/launcher/tk/page/page_frame.py
+++ src/launcher/tk/page/page_frame.py
@@ -6,3 +6,6 @@ class PageFrame(ttk.Frame):
         ttk.Frame.__init__(self, parent_frame)
         self.configure(width=parent_frame["width"], height=parent_frame["height"])
         self.parent_frame: ttk.Frame = parent_frame
+
+    def handle_exit(self) -> None:
+        assert False, "NOT IMPLEMENTED"
