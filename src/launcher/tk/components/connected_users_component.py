4a69260845ce2bb990fab37aea9e689844ab8252
Nicolas <nicolas.parucker@hotmail.com>
2023-06-21 10:12:46 +0200

    actually first working implementation of connected user component

--- src/launcher/tk/components/connected_users_component.py
+++ src/launcher/tk/components/connected_users_component.py
@@ -6,6 +6,10 @@ from ttkbootstrap import ttk
 
 from config.tk_config import CANVAS_WINDOW_POS
 from config.tk_config import SCROLLBAR_WIDTH
+from config.tk_config import USER_CARD_SPACING
+from config.tk_config import CONNECTED_USER_PAD
+from config.tk_config import DEFAULT_CARD_HEIGHT
+from config.tk_config import MAX_CONNECTIONS
 from launcher.tk.components.tk_component import Component
 from launcher.tk.global_vars import GlobalUserVars
 from launcher.tk.launcher_user import LauncherUser
@@ -23,6 +27,7 @@ class ConnectedUserCard(typing.NamedTuple):
     user_name: str
     card_frame: ttk.Frame
     select_button: ttk.Button
+    elo_label: ttk.Label
 
 
 class CanvasWindowRect(typing.NamedTuple):
@@ -52,14 +57,15 @@ class ConnectedUsersComponent(Component):
         self.widgets.container.grid(sticky=tk.NSEW, pady=20, padx=20)
 
     def create_widgets(self) -> ConnectedUsersWidgets:
+        window_frame_height: int = MAX_CONNECTIONS * DEFAULT_CARD_HEIGHT
         container: ttk.Frame = ttk.Frame(self.frame)
         scroll_bar: ttk.Scrollbar = ttk.Scrollbar(container, orient=tk.VERTICAL, style=ttkbootstrap.WARNING)
         canvas: tk.Canvas = tk.Canvas(container)
-        window_frame: ttk.Frame = ttk.Frame(container)
+        window_frame: ttk.Frame = ttk.Frame(container, height=window_frame_height)
         window_id: int = canvas.create_window(*CANVAS_WINDOW_POS, window=window_frame, anchor=tk.NW)
         canvas.configure(yscrollcommand=scroll_bar.set)
         scroll_bar.configure(command=canvas.yview)
-        container.bind("<Configure>", self.handle_container_resize)
+        container.bind("<Configure>", lambda event: self.handle_container_resize(event.width))
         return ConnectedUsersWidgets(container, scroll_bar, canvas, window_frame, window_id)
 
     def handle_exit(self) -> None:
@@ -68,36 +74,53 @@ class ConnectedUsersComponent(Component):
     def get_canvas_window_rect(self) -> CanvasWindowRect:
         return CanvasWindowRect(*self.widgets.canvas.bbox(self.widgets.window_id))
 
-    def handle_container_resize(self, event) -> None:
-        canvas_window_rect: CanvasWindowRect = self.get_canvas_window_rect()
-        if event.height > canvas_window_rect.height:
-            self.widgets.canvas.itemconfig(self.widgets.window_id, height=event.height)
-            self.widgets.canvas.configure(scrollregion=(0, 0, 0, event.height))
-
-        self.widgets.canvas.itemconfig(self.widgets.window_id, width=event.width - SCROLLBAR_WIDTH)
+    def handle_container_resize(self, event_width: int) -> None:
+        # setting the size of the window_frame everytime we resize container. seems to be the
+        # only way of prevent the created_window from shrinking to children combined height
+        self.widgets.canvas.itemconfig(self.widgets.window_id, height=MAX_CONNECTIONS * 100)
+        self.widgets.canvas.itemconfig(self.widgets.window_id, width=event_width - SCROLLBAR_WIDTH)
 
     def update_connected_users(self) -> None:
-        users: list[str] = GlobalUserVars.get().get_var(GlobalUserVars.connected_users).get().split()
-        users = list(filter(lambda u_name: u_name != LauncherUser.get_user().u_name, users))
-        self.pack_forget_user_cards()
-        self.user_cards = self.create_user_cards(users)
+        users_info: list[tuple[str, str]] = []
+        for user_info in GlobalUserVars.get().get_var(GlobalUserVars.connected_users).get().split():
+            user_name, user_elo = user_info.split("-")
+            if user_name == LauncherUser.get_user().u_name: continue
+            users_info.append((user_name, user_elo))
+
+        self.destroy_user_cards()
+        self.user_cards = self.create_user_cards(users_info)
         self.pack_user_cards()
 
-    def pack_forget_user_cards(self) -> None:
+    def destroy_user_cards(self) -> None:
         for user_card in self.user_cards:
-            user_card.card_frame.pack_forget()
+            user_card.card_frame.destroy()
 
-    def create_user_cards(self, users: list[str]) -> list[ConnectedUserCard]:
+    def create_user_cards(self, users_info: list[tuple[str, str]]) -> list[ConnectedUserCard]:
         connected_users: list[ConnectedUserCard] = []
-        for user_name in users:
+        for user_info in users_info:
+            user_name, user_elo = user_info
             card_frame: ttk.Frame = ttk.Frame(self.widgets.window_frame)
             select_button: ttk.Button = ttk.Button(card_frame, text=user_name)
-            connected_users.append(ConnectedUserCard(user_name, card_frame, select_button))
+            elo_label: ttk.Label = ttk.Label(card_frame, text=f"elo : {user_elo}")
+            connected_users.append(
+                ConnectedUserCard(user_name, card_frame, select_button, elo_label)
+            )
         return connected_users
+
+    def get_scroll_region_height(self) -> int:
+        self.widgets.window_frame.update()
+        height: int = USER_CARD_SPACING * (len(self.widgets.window_frame.children.values()) + 1)
+        for child in self.widgets.window_frame.children.values():
+            height += child.winfo_height()
+        return height
+
+    def update_scroll_region(self) -> None:
+        height: int = self.get_scroll_region_height()
+        self.widgets.canvas.configure(scrollregion=(0, 0, 0, height))
 
     def pack_user_cards(self) -> None:
         for user_card in self.user_cards:
-            user_card.select_button.pack(expand=True)
-            user_card.card_frame.pack(expand=True)
-        self.widgets.window_frame.update()
-        self.widgets.canvas.configure(scrollregion=(0, 0, 0, self.widgets.window_frame.winfo_height()))
+            user_card.elo_label.pack(side=tk.LEFT, expand=True)
+            user_card.select_button.pack(side=tk.LEFT, expand=True)
+            user_card.card_frame.pack(padx=CONNECTED_USER_PAD, pady=USER_CARD_SPACING)
+        self.update_scroll_region()
