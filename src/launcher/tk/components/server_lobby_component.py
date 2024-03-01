aae6099d09622e9900af25d17b692f37c80b92e8
Nicolas <nicolas.parucker@hotmail.com>
2024-03-01 12:01:41 +0700

    Added simple button allowing you to join the q

--- src/launcher/tk/components/server_lobby_component.py
+++ src/launcher/tk/components/server_lobby_component.py
@@ -1,8 +1,18 @@
+import time
+
 from ttkbootstrap import ttk
 
 from launcher.tk.components.tk_component import Component
+from launcher.tk.launcher_user import LauncherUser
 
 
 class ServerLobbyComponent(Component):
     def __init__(self, parent: ttk.Frame) -> None:
         super().__init__(parent, "Lobby")
+
+        queue: ttk.Button = ttk.Button(self.frame, text="Queue for Match", command=enter_queue)
+        queue.pack()
+
+
+def enter_queue() -> None:
+    LauncherUser.get_client().send_queue_request()
