cf8315a28968f5b43d032e74bc8d9aade665d459
Nicolas <nicolas.parucker@hotmail.com>
2024-03-03 09:58:20 +0700

    Added function to get the opposite side

--- src/chess/board/side.py
+++ src/chess/board/side.py
@@ -1,6 +1,19 @@
+from __future__ import annotations
 import enum
 
 
 class Side(enum.Enum):
     WHITE = enum.auto()
     BLACK = enum.auto()
+
+    def get_opposite(self) -> Side:
+        side: Side = Side[self.name]
+
+        if side is Side.WHITE:
+            return Side.BLACK
+
+        if side is Side.BLACK:
+            return Side.WHITE
+
+        else:
+            raise Exception
